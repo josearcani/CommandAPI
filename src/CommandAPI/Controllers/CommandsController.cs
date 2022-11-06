41ec4b6637b0889efdd83d8fe4ffc7e064692620
Jose Arcani <juanjose.arcani2309@gmail.com>
2022-11-06 09:06:03 -0400

    add mock data layer

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,3 +1,5 @@
+using CommandAPI.Data;
+using CommandAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandAPI.Controllers;
@@ -6,9 +8,30 @@ namespace CommandAPI.Controllers;
 [ApiController]
 public class CommandsController : ControllerBase
 {
-    [HttpGet]
-    public ActionResult<IEnumerable<string>> Get()
+    private readonly ICommandAPIRepo _repository;
+    public CommandsController(ICommandAPIRepo repository)
     {
-        return new string[] { "this", "is", "a", "test" };
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Command>> GetAllCommands()
+    {
+        var commandItems = _repository.GetAllCommands();
+
+        return Ok(commandItems);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Command> GetCommandById(int id)
+    {
+        var commandItem = _repository.GetCommandById(id);
+
+        if (commandItem == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(commandItem);
     }
 }
--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,6 +1,11 @@
+using CommandAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+// dependecy injection
+builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
 
 var app = builder.Build();
 
