54086a6e19c5495cd0c8f3e1b23a8249a9f4914d
Jose Arcani <juanjose.arcani2309@gmail.com>
2022-11-06 08:05:14 -0400

    initial commit

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CommandsController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+        return new string[] { "this", "is", "a", "test" };
+    }
+}
