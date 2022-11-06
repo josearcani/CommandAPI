396c34feea299007604f72805cb0a3ea62bb8c84
Jose Arcani <juanjose.arcani2309@gmail.com>
2022-11-06 14:44:02 -0400

    add db connection

--- src/CommandAPI/Data/ICommandAPIRepo.cs
+++ src/CommandAPI/Data/ICommandAPIRepo.cs
@@ -5,7 +5,7 @@ public interface ICommandAPIRepo
 {
     bool SaveChanges();
     IEnumerable<Command> GetAllCommands();
-    Command GetCommandById(int id);
+    Command? GetCommandById(int id);
     void CreateCommand(Command cmd);
     void UpdateCommand(Command cmd);
     void DeleteCommand(Command cmd);
--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,11 +1,16 @@
 using CommandAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<CommandContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
 
 builder.Services.AddControllers();
 
 // dependecy injection
-builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+builder.Services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
+// builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
 
 var app = builder.Build();
 
