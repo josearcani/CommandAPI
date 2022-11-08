644ee2d6629e5b29d59273bd8d2d6515c7ce687a
Jose Arcani <juanjose.arcani2309@gmail.com>
2022-11-07 22:37:41 -0400

    use secret.json to not upload credentials to github

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,10 +1,16 @@
 using CommandAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var stringBuilder = new NpgsqlConnectionStringBuilder();
+stringBuilder.ConnectionString = builder.Configuration.GetConnectionString("PostgreSqlConnection");
+stringBuilder.Username = builder.Configuration["UserID"];
+stringBuilder.Password = builder.Configuration["Password"];
+
 builder.Services.AddDbContext<CommandContext>(opt =>
-    opt.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
+    opt.UseNpgsql(stringBuilder.ConnectionString));
 
 builder.Services.AddControllers();
 
